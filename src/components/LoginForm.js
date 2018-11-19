62970624ae4dab48cd97cc4dc57587617930c21c
Kim Minyeob <stankim0525@gmail.com>
2018-11-19 16:24:46 +0900

    a

---
+++ src/components/LoginForm.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import api from '../api'
+
+export default class LoginForm extends React.Component {
+    async handleSubmit(e){
+        e.preventDefault()
+        const username = e.target.elements.username.value
+        const password = e.target.elements.password.value
+        const res = await api.post('/users/login', {
+            username,
+            password
+    })
+    localStorage.setItem('token', res.data.token)
+    // TODO: 게시글 목록 보여주기
+
+    }
+
+  render() {
+    const { onRegister } = this.props;
+    return (
+        <React.Fragment>
+      <form onSubmit={e => this.handleSubmit(e)}>
+        <h1>Login</h1>
+        <input type="text" name="username" />
+        <input type="password" name="password" />
+        
+        <button >login</button>
+      </form>
+        <button onClick={() => onRegister()}>sign up</button>
+        </React.Fragment>
+    );
+  }
+}
---
+++ src/components/RegisterForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import api from '../api'
+
+
+export default class RegisterForm extends Component {
+async handleSubmit(e){
+    e.preventDefault()
+    const username = e.target.elements.username.value
+    const password = e.target.elements.password.value
+
+    //FIXME: 사용자 이름 중복 체크 해야함
+    const {data: users} = await api.get('/users',{
+      params:{
+        username
+      }
+    })
+      if(users.length > 0){
+        alert('이미있어')
+        return
+      }
+    const res = await api.post('/users/register', {
+        username,
+        password
+    })
+    localStorage.setItem('token', res.data.token)
+    // TODO: 게시글 목록 보여주기
+}
+
+
+  render() {
+    return (
+      <form onSubmit={e => this.handleSubmit(e)}>
+        <h1>sign up</h1>
+        <input type="text" name="username" />
+        <input type="password" name="password" />
+        <button>go!</button>
+      </form>
+    );
+  }
+}
