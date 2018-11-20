88a3a55c3fa4ad222789fceb19d147d55fde4e26
Kim Minyeob <stankim0525@gmail.com>
2018-11-20 18:34:21 +0900

    can be edit ur new post mayde?

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,30 +1,35 @@
 import React, { Component } from 'react'
-import api from '../api'
+import api from '../api';
 
 export default class PostDetail extends Component {
-    constructor(props){
-        super(props)
-        this.state ={
-            body:'',
-            title:''
-        }
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      body: '',
+      title: ''
     }
-    async componentDidMount () {
-        const {data: {title, body} }= await api.get(`/posts/${this.props.postId}`)
-           this.setState({
-               title,
-               body
-           })
-      }
-      
+  }
+  
+  async componentDidMount() {
+    const {data: {title, body}} = await api.get(`/posts/${this.props.postId}`)
+    this.setState({
+      title,
+      body
+    })
+  }
+  
   render() {
-    const {postId}= this.props
+    const {postId, onEditPostFormPage} = this.props
     const {title, body} = this.state
+
     return (
       <div>
-        <h1>Title: {title}</h1>
-        <h2>Body: {body}</h2>
-        {postId}
+        <h1>게시물</h1>
+        <button onClick={() => onEditPostFormPage(postId)}>수정</button>
+        <div>{title}</div>
+        <div>{body}</div>
       </div>
     )
   }
